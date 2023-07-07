10849f5497cb0099028981b9e933951d0275d814
Anton Berezhnyi <aberezhniy@clips4sale.com>
2023-07-07 15:46:19 +0300

    Make to reach 100% test coverage

--- main.go
+++ main.go
@@ -1,75 +1,12 @@
+//go:build !test
+
 package main
 
 import (
-	"fmt"
-	"github.com/berejant/go-kneu"
-	"github.com/gin-gonic/gin"
-	"github.com/kneu-messenger-pigeon/events"
-	"github.com/segmentio/kafka-go"
-	"io"
 	"net/http"
 	"os"
 )
 
-const ExitCodeMainError = 1
-
-type httpInterface interface {
-	ListenAndServe(addr string, handler http.Handler) error
-}
-
 func main() {
 	os.Exit(handleExitError(os.Stderr, runApp(os.Stdout, http.ListenAndServe)))
-}
-
-func runApp(out io.Writer, listenAndServe func(string, http.Handler) error) error {
-	envFilename := ""
-	if _, err := os.Stat(".env"); err == nil {
-		envFilename = ".env"
-	}
-
-	config, err := loadConfig(envFilename)
-	if err != nil {
-		return err
-	}
-
-	gin.SetMode(gin.ReleaseMode)
-
-	apiController := &ApiController{
-		out:    out,
-		config: config,
-		writer: &kafka.Writer{
-			Addr:     kafka.TCP(config.kafkaHost),
-			Topic:    events.AuthorizedUsersTopic,
-			Balancer: &kafka.LeastBytes{},
-		},
-		oauthClient: &kneu.OauthClient{
-			BaseUri:      config.kneuBaseUri,
-			ClientId:     config.kneuClientId,
-			ClientSecret: config.kneuClientSecret,
-		},
-		apiClientFactory: func(token string) kneu.ApiClientInterface {
-			return &kneu.ApiClient{
-				BaseUri:     config.kneuBaseUri,
-				AccessToken: token,
-			}
-		},
-	}
-
-	apiController.apiClientFactory("test")
-
-	return listenAndServe(
-		config.listenAddress, apiController.setupRouter(),
-	)
-}
-
-func handleExitError(errStream io.Writer, err error) int {
-	if err != nil {
-		_, _ = fmt.Fprintln(errStream, err)
-	}
-
-	if err != nil {
-		return ExitCodeMainError
-	}
-
-	return 0
 }
